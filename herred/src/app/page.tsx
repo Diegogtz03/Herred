06a4b8a7d18c3c7a974b50a15c015ecdec3684f3
Diego Gtz <diego_gtz_t@hotmail.com>
2025-05-14 09:32:17 -0600

    add: Added header and TlDraw

--- herred/src/app/page.tsx
+++ herred/src/app/page.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { Tldraw } from "tldraw";
+import { useState } from "react";
+import Header from "@/components/layout/Header";
+import BodyTemplate from "@/components/layout/BodyTemplate";
+import Sidebar from "@/components/sidebar/Sidebar";
 import "tldraw/tldraw.css";
 
 export default function Home() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
-    <div>
-      <Tldraw />
+    <div className="h-full w-full flex flex-col">
+      <Header
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <BodyTemplate isSidebarOpen={isSidebarOpen} sidebarContent={<Sidebar />}>
+        <Tldraw />
+      </BodyTemplate>
     </div>
   );
 }
