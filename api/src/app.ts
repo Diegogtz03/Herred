88d043ef4691413c5db78555f5b62879e022ea2a
Roberto García <A01284731@tec.mx>
2025-05-14 21:34:49 -0600

    BFS implemented with simple POST route

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,14 @@
-import express, { Request, Response } from 'express';
-import exampleRoutes from './routes/example';
+import express, { Request, Response } from "express";
+import exampleRoutes from "./routes/example";
+var bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/api', exampleRoutes);
+app.use(express.json());
+app.use(bodyParser.json()); // for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", exampleRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- api/src/routes/example.ts
+++ api/src/routes/example.ts
@@ -1,8 +1,10 @@
-import { Router } from 'express';
-import { getExample } from '../controllers/example';
+import { Router } from "express";
+import { getExample } from "../controllers/example";
+import { routeCalc } from "../controllers/routeCalc";
 
 const router = Router();
 
-router.get('/example', getExample);
+router.get("/example", getExample);
+router.post("/calcRoutes", routeCalc);
 
 export default router;
