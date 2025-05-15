ae79fad4a5215d11d76e5a85a7e024801d3bc3b5
Diego Gtz <diego_gtz_t@hotmail.com>
2025-05-15 15:41:13 -0600

    change: added node to canvas

--- herred/src/components/tools/node/NodeTool.ts
+++ herred/src/components/tools/node/NodeTool.ts
@@ -1,4 +1,6 @@
-import { StateNode } from "tldraw";
+import { AssetRecordType, StateNode } from "tldraw";
+
+const OFFSET = 25
 
 export class NodeTool extends StateNode {
   static override id = 'node-selection'
@@ -7,4 +9,42 @@ export class NodeTool extends StateNode {
   override onEnter() {
     this.editor.setCursor({ type: 'cross', rotation: 0 })
   }
+
+  override onPointerDown() {
+		const { currentPagePoint } = this.editor.inputs
+
+    const assetId = AssetRecordType.createId()
+    const imageWidth = 50
+    const imageHeight = 50
+
+    this.editor.createAssets([
+			{
+				id: assetId,
+				type: 'image',
+				typeName: 'asset',
+				props: {
+					name: 'node.svg',
+					src: '/icons/tools/node.svg',
+					w: imageWidth,
+					h: imageHeight,
+					mimeType: 'image/svg+xml',
+					isAnimated: false,
+				},
+				meta: {},
+			},
+		])
+
+		this.editor.createShape({
+			type: 'image',
+			x: currentPagePoint.x - OFFSET,
+			y: currentPagePoint.y - OFFSET,
+			props: {
+				assetId,
+				w: imageWidth,
+				h: imageHeight,
+			},
+		})
+
+    console.log('Added node')
+	}
 }
