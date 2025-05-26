ed5869cc487ab4bdb96b9d16212a56cefbcf9e2a
joscaz <joscazertuche@gmail.com>
2025-05-25 21:16:15 -0600

    Partial addition of connection types

--- herred/src/components/tools/CustomStylePanel.tsx
+++ herred/src/components/tools/CustomStylePanel.tsx
@@ -25,12 +25,29 @@ const NODE_TYPES = [
   },
 ];
 
+const CONNECTION_TYPES = [
+  {
+    type: "fiber",
+    icon: "/icons/tools/fiber_connection.svg",
+  },
+  {
+    type: "microwave",
+    icon: "/icons/tools/micro_connection.svg",
+  }
+]
+
 export const myNodeStyle = StyleProp.defineEnum("myNodeStyle", {
   defaultValue: "central",
   values: ["central", "leaf", "proposal"],
 });
 
+export const myConnectionStyle = StyleProp.defineEnum("myConnectionStyle", {
+  defaultValue: "fiber",
+  values: ["fiber", "microwave"],
+});
+
 type MyNodeStyle = T.TypeOf<typeof myNodeStyle>;
+type MyConnectionStyle = T.TypeOf<typeof myConnectionStyle>;
 
 export type NodeShape = TLBaseShape<
   "node",
@@ -41,6 +58,18 @@ export type NodeShape = TLBaseShape<
   }
 >;
 
+export type ConnectionShape = TLBaseShape<
+  "connection",
+  {
+    color: string;
+    dash: string;
+    size: string;
+    arrowheadStart: string;
+    arrowheadEnd: string;
+    connectionType: MyConnectionStyle;
+  }
+>;
+
 export function CustomStylePanel() {
   const editor = useEditor();
   const relevantStyles = useRelevantStyles();
@@ -48,10 +77,12 @@ export function CustomStylePanel() {
   if (!relevantStyles) return null;
 
   const nodeStyle = relevantStyles.get(myNodeStyle);
+  const connectionStyle = relevantStyles.get(myConnectionStyle);
 
   return (
     <DefaultStylePanel>
       {nodeStyle !== undefined && <NodePicker editor={editor} />}
+      {connectionStyle !== undefined && <ConnectionTypePicker editor={editor} />}
     </DefaultStylePanel>
   );
 }
@@ -78,4 +109,51 @@ function NodePicker({ editor }: { editor: Editor }) {
       ))}
     </div>
   );
+}
+
+function ConnectionTypePicker({ editor }: { editor: Editor}) {
+  return (
+    <div className="w-full">
+      {CONNECTION_TYPES.map((type, index) => (
+        <div key={type.type} className="flex flex-col justify-center gap-1">
+          {index > 0 && <div className="h-px bg-gray-200" />}
+          <button
+            key={type.type}
+            className="flex items-center gap-1 p-1 rounded-md hover:bg-gray-100 transition-colors duration-200 m-1 cursor-pointer active:bg-gray-200"
+            onClick={() => {
+              editor.markHistoryStoppingPoint();
+              const value = type.type as MyConnectionStyle;
+              editor.setStyleForSelectedShapes(myConnectionStyle, value);
+              const selectedShapes = editor.getSelectedShapes().filter(s => s.type === 'connection');
+              selectedShapes.forEach(shape => {
+                if (shape.type === 'connection') {
+                  let newColor = 'black';
+                  let newDash: "draw" | "dotted" | "dashed" | "solid" = "solid";
+                  if (value === 'fiber') {
+                    newColor = 'yellow';
+                    newDash = 'solid';
+                  } else if (value === 'microwave') {
+                    newColor = 'black';
+                    newDash = 'dashed';
+                  }
+                  editor.updateShape({
+                    id: shape.id,
+                    type: "connection",
+                    props: {
+                      ...shape.props,
+                      color: newColor,
+                      dash: newDash,
+                    }
+                  })
+                }
+              })
+            }}
+          >
+            <Image src={type.icon} alt={type.type} width={34} height={34}/>
+            <span className="text-[14px] text-gray-500">{type.type}</span>
+          </button>
+        </div>
+      ))}
+    </div>
+  )
 }
